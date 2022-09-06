94c76e61d1344042888a1a721bfc4d52c55c47d3
Osvaldo Gac <oagacp@gmail.com>
2022-09-06 18:59:56 -0300

    Add user table, user model and new branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_232654) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_203318) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_232654) do
     t.datetime "updated_at", null: false
     t.datetime "create_at"
     t.datetime "update_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "fname"
+    t.string "lname"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
