beac572d9429318b07f0a9ef21215b86309a6644
Osvaldo Gac <oagacp@gmail.com>
2022-09-07 14:53:26 -0300

    email validation and using the association article-user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 25}
   validates :fname, presence: true
   validates :lname, presence: true
-  VALID_EMAIL_REGEX =/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 105},
             format: { with: VALID_EMAIL_REGEX}
 end
