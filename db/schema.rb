83bf3c8405fadee5357276c480f3eb62b0cd6581
Osvaldo Gac <oagacp@gmail.com>
2022-09-06 21:00:44 -0300

    one to many assoociation between user ans articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_203318) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_232532) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_06_203318) do
     t.datetime "updated_at", null: false
     t.datetime "create_at"
     t.datetime "update_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
