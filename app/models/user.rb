8e185a759475191bcc32634212b4e444b6b54bea
Osvaldo Gac <oagacp@gmail.com>
2022-09-07 18:41:53 -0300

    Include the bcrypt gem and add to the model the field password

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 105},
             format: { with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
