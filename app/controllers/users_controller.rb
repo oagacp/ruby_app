28af5671fd3bb99a2cf0f3b512373165fde2993a
Osvaldo Gac <oagacp@gmail.com>
2022-09-07 21:16:07 -0300

    Creation user action and sign up button in home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        flash[:notice] = "Welcome to Ruby Blog #{@user.username}, you have successfully sign up."
+        redirect_to articles_path
+      else
+        render :new
+      end
+    end
+  end
 end
+
+private
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:username, :fname, :lname, :email, :password)
+  end
